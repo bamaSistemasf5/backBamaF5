7b43cb3f6a4e1cc87f1a5b7726fa637f42bf4836
Lea Montoya <leamontoya19@gmail.com>
2024-03-21 10:27:21 +0100

    create/delete-invoices files related deleted and jsPDF dependency added

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -1,15 +1,6 @@
 import e from 'express';
 import InvoiceModel from '../models/invoiceModel.js';
 
-export const createInvoice= async (req, res) => {
-  try {
-    const newInvoice = await InvoiceModel.create(req.body);
-    res.status(201).json(newInvoice);
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ error: error.message });
-  }
-};
 
 export const invoicesView = async (req, res) => {
   try {
@@ -35,42 +26,28 @@ export const getInvoiceById = async (req, res) => { // Cambia el nombre de la fu
     res.status(500).json({ error: 'Error al obtener la factura' });
   }
 };
-export const updateInvoice = async (req, res) => {
-  const { id } = req.params; 
-  console.log (id)
-  try {
-    const [updated] = await InvoiceModel.update(req.body, {
-      where: { nro_factura: id } // Utiliza cif_cliente como el identificador único
-    });
-    if (updated) {
-      const updatedInvoice = await InvoiceModel.findOne({ where: { nro_factura: id } });
-      res.json(updatedInvoice);
-    } else {
-      res.status(404).json({ error: 'Factura no encontrada' });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Error al actualizar la factura' });
-  }
-}
+
+// export const updateInvoice = async (req, res) => {
+//   const { id } = req.params; 
+//   console.log (id)
+//   try {
+//     const [updated] = await InvoiceModel.update(req.body, {
+//       where: { nro_factura: id } 
+//     });
+//     if (updated) {
+//       const updatedInvoice = await InvoiceModel.findOne({ where: { nro_factura: id } });
+//       res.json(updatedInvoice);
+//     } else {
+//       res.status(404).json({ error: 'Factura no encontrada' });
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ error: 'Error al actualizar la factura' });
+//   }
+// }
 
 
 
         
 
-export const deleteInvoice = async (req, res) => {
-  const { id } = req.params;
-  //console.log(id);
-  try {
-    const invoice = await InvoiceModel.findByPk(id);
-    if (invoice) {
-      await invoice.destroy();
-      res.json(invoice);
-    } else {
-      res.status(404).json({ error: 'Factura no encontrada' });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Error al eliminar la factura' });
-  }
-}
+
