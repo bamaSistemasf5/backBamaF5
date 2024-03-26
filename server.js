af575c546f315b8785369d271da1abf17d7dda18
AlejoxVargas <henrryalejandro21@gmail.com>
2024-03-26 02:39:49 +0100

    Tests in Jest for controllers, database and models

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-import express from 'express';
-import cors from 'cors';
-import db from './database/db.js';
+import express from "express";
+import cors from "cors";
+import db from "./database/db.js";
 
-import clientRoutes from './routes/clientRoutes.js';
-import deliveryNotesRoutes from './routes/deliveryNotesRoutes.js';
-import loginRoutes from './routes/loginRoutes.js';
-import ordersRoutes from './routes/ordersRoutes.js';
-import invoicesViewRoute from './routes/invoicesViewRoute.js';
+import clientRoutes from "./routes/clientRoutes.js";
+import deliveryNotesRoutes from "./routes/deliveryNotesRoutes.js";
+import loginRoutes from "./routes/loginRoutes.js";
+import ordersRoutes from "./routes/ordersRoutes.js";
+import invoicesViewRoute from "./routes/invoicesViewRoute.js";
 
 const app = express();
-
-const PORT = 3000;
 
 // Configurar el middleware CORS
 app.use(cors());
 
 app.use(express.json());
+
 app.use("/", loginRoutes);
 app.use("/client", clientRoutes);
 app.use("/delivery-note", deliveryNotesRoutes);
@@ -25,17 +24,17 @@ app.use("/order", ordersRoutes);
 app.get("/invoices-view", invoicesViewRoute);
 // app.post('/invoices-view/:nro_factura/download', downloadInvoicePDF);
 
-//app.post("/create-invoices", createInvoiceRoute); 
-// app.get("/invoices/:id", getInvoiceRoute); 
-// app.put("/invoices/:id", updateInvoiceRoute); 
+//app.post("/create-invoices", createInvoiceRoute);
+// app.get("/invoices/:id", getInvoiceRoute);
+// app.put("/invoices/:id", updateInvoiceRoute);
 // app.delete("/invoices/:id", deleteInvoiceRoute);
-try {
-  await db.authenticate();
-  console.log("Connection has been established successfully.");
-} catch (error) {
-  console.log(error);
+
+export function startServer() {
+  const port = process.env.NODE_ENV === 'test' ? 8001 : 8000;
+  const server = app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+  return server;
 }
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
